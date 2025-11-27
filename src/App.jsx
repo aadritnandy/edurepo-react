fb03484dd40e73f770b4e28f7b513ed4b8fdb312
aadritnandy <aadritnandy10@gmail.com>
2025-11-27 12:22:59 -0800

    Update package

--- src/App.jsx
+++ src/App.jsx
@@ -143,7 +143,7 @@ const AppContent = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/edurepo-react">
       <AuthProvider>
         <AppContent />
       </AuthProvider>
